b176b7e725e8c594b68898a3c70711e08b0aab12
JordanComas <jordancomas1@yahoo.com>
2022-09-27 16:01:25 -0400

    put cors back

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-// const cors = require("cors");
+const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv/config");
 
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(cors());
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
